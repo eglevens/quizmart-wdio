cd3c7736458fa47aa9c21bae1eb96b51f093717a
Eglė Venskūnaitė <egle.venskunaite@telesoftas.com>
2022-01-05 14:15:16 +0200

    mocha dependency and tests refactor

--- test-suites/login.e2e.js
+++ test-suites/login.e2e.js
@@ -1,20 +1,25 @@
-const LoginPage = require('../pageobjects/login.page');
-const DiscoverPage = require('../pageobjects/discover.page');
+const LoginPage = require('../web/src/pages/login.page');
+const DiscoverPage = require('../web/src/pages/discover.page');
+
+const email = 'eglvns@telesoftas.com';
+const pass = 'myPasswordIs321'
+const invalidPass = 'myPasswordIs31'
 
 describe('Login cases', () => {
+
+    beforeEach(function(){
+        LoginPage.open();
+    });
+
     it('should login with valid credentials and be redirected to discover page', async () => {
-        await LoginPage.open();
-        await LoginPage.login('eglvns@telesoftas.com', 'myPasswordIs321');
-        
+        await LoginPage.login(email, pass);
         await expect(browser).toHaveUrlContaining('discover');
         await expect(DiscoverPage.headerTitle).toBeDisplayed();
         
     });
 
     it('should stay on login with invalid password', async () => {
-        await LoginPage.open();
-        await LoginPage.login('eglvns@telesoftas.com', 'myPasswordIs31');
-
+        await LoginPage.login(email, invalidPass);
         await expect(LoginPage.errInvalidPass).toBeDisplayed();
     });
 
