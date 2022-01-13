7c50f9f143745345668833a8f0a7181d702a2d61
Eglė Venskūnaitė <egle.venskunaite@telesoftas.com>
2022-01-13 16:48:25 +0200

    registration test cases with implementation

--- web/pageObjects/register.page.ts
+++ web/pageObjects/register.page.ts
@@ -2,8 +2,69 @@ import * as page from './page'
 
 
 const registerPageTitle = '//h1[text()="Register"]'
+const emailInput = '//input[@placeholder="Email" and @name="email"]'
+const passInput = '//input[@name="password"]'
+const repeatPassInput = '//input[@name="passwordRepeat"]'
+const signUpBtn = '//button[text()="Sign up"]'
+const signInBtn = '//h3[text()="Already have account?"]/../*[text()="Sign in"]'
+const newsletterCheckbox = '//div[text()="I would like to receive"]/../button[@type="button"]'
+const userAlreadyExistsError = '//h3[text()="User with this email already exists."]'
+const emailFormatValidationError = '//input[@name="email"]/../h3[text()="Must be valid email"]'
+const passwordlengthValidationError = '//input[@name="password"]/../h3[text()="Password must be at least of 6 characters in length"]'
+const notMatchingRepeatPasswordValidationError = '//input[@name="passwordRepeat"]/../h3[text()="Passwords must match"]'
+const emailRequiredValidationError = '//input[@name="email"]/../h3[text()="Required"]'
+const passRequiredValidationError = '//input[@name="password"]/../h3[text()="Required"]'
+const repeatPassRequiredValidationError = '//input[@name="passwordRepeat"]/../h3[text()="Required"]'
 
+
+
+export async function openRegisterPage(): Promise<void> {
+    await browser.url('sign-up/')
+}
+
+export async function registerWithEmail(email: string, pass: string, repeatPass: string): Promise<void>{
+    await page.sendValueByLocator(emailInput, email)
+    await page.sendValueByLocator(passInput, pass)
+    await page.sendValueByLocator(repeatPassInput, repeatPass)
+    await page.clickByLocator(signUpBtn)
+}
+
+export async function clickSignUpBtn(): Promise<void> {
+    await page.clickByLocator(signUpBtn)
+}
+
+export async function clickSignInBtn(): Promise<void> {
+    await page.clickByLocator(signInBtn)
+}
 
 export async function getRegisterPageTitle(): Promise<boolean> {
     return await page.elementPresentByLocator(registerPageTitle) 
+}
+
+export async function getEmailFormatValidationError(): Promise<boolean> {
+    return await page.elementPresentByLocator(emailFormatValidationError) 
+}
+
+export async function getPasswordLengthValidationError(): Promise<boolean> {
+    return await page.elementPresentByLocator(passwordlengthValidationError) 
+}
+
+export async function getNotMatchingRepeatPasswordValidationError(): Promise<boolean> {
+    return await page.elementPresentByLocator(notMatchingRepeatPasswordValidationError) 
+}
+
+export async function getUserAlreadyExistsError(): Promise<boolean> {
+    return await page.elementPresentByLocator(userAlreadyExistsError) 
+}
+
+export async function getEmailRequiredValidationError(): Promise<boolean> {
+    return await page.elementPresentByLocator(emailRequiredValidationError)
+}
+
+export async function getPassRequiredValidationError(): Promise<boolean> {
+    return await page.elementPresentByLocator(passRequiredValidationError)
+}
+
+export async function getRepeatPassRequiredValidationError(): Promise<boolean> {
+    return await page.elementPresentByLocator(repeatPassRequiredValidationError)
 }
--- web/tests/registerEmail-e2e.ts
+++ web/tests/registerEmail-e2e.ts
@@ -1,11 +1,10 @@
 import * as registerPage from "../pageObjects/register.page"
 import { expect } from 'chai'
+import { OperationCanceledException } from "typescript"
 
 
 const email = 'eglvns@telesoftas.com'
 const pass = 'myPasswordIs321'
-const invalidPass = 'myPasswordIs'
-const unregistredEmail = 'eglvns+unregistered@telesoftas.com'
 const invalidFormatEmail = 'eglvnstelesoftas.com'
 const shortPass = '12345'
 
@@ -13,7 +12,7 @@ const shortPass = '12345'
 describe('Register with email from register page', () => {
 
     beforeEach(function(){
-        registerPage.openSignUp()
+        registerPage.openRegisterPage()
     })
 
     it('Success registration without newsletter', async () => {
@@ -23,15 +22,22 @@ describe('Register with email from register page', () => {
     })
 
     it('Attempt to register with already registered email', async() => {
+        await registerPage.registerWithEmail(email, pass, pass)
+        expect (await registerPage.getUserAlreadyExistsError()).to.be.true
     })
 
-    it('Validation error with invalid email format and too short password', async () => {
-    })
-
-    it('Validation error with not matching password', async () => {
+    it('Validation error with invalid email format, too short password and not matching repeat password', async () => {
+        await registerPage.registerWithEmail(invalidFormatEmail, shortPass, shortPass)
+        expect (await registerPage.getEmailFormatValidationError()).to.be.true
+        expect (await registerPage.getPasswordLengthValidationError()).to.be.true
+        expect (await registerPage.getNotMatchingRepeatPasswordValidationError()).to.be.true
     })
 
     it('Validation error with empty email & passwords', async () => {
+        await registerPage.clickSignUpBtn()
+        expect (await registerPage.getEmailRequiredValidationError()).to.be.true
+        expect (await registerPage.getPassRequiredValidationError()).to.be.true
+        expect (await registerPage.getRepeatPassRequiredValidationError()).to.be.true
     })
 
 })
