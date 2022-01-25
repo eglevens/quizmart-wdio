c962d0911e1e864c1faa60025fa790ceec9e5860
Eglė Venskūnaitė <egle.venskunaite@telesoftas.com>
2022-01-25 13:54:12 +0200

    add waiting function in login with google flow

--- web/pageObjects/signInGoogle.ts
+++ web/pageObjects/signInGoogle.ts
@@ -8,11 +8,17 @@ const nextBtn = '//button //span[text()="Kitas"]'
 
 //----------------ACTION----------------
 
-export async function loginWithGoogle(email, pass): Promise<void> {
+export async function continueWithGoogle(email: string, pass: string): Promise<void> {
     await page.waitUntilElementIsClickableByLocator(nextBtn, 10000)
     await page.sendValueByLocator(emailInput, email)
     await page.clickByLocator(nextBtn)
     await page.waitUntilElementIsClickableByLocator(nextBtn, 10000)
     await page.sendValueByLocator(passInput, pass)
     await page.clickByLocator(nextBtn)
+}
+
+//----------------WAIT----------------
+
+export async function waitForNextBtnInGoogleIsClickable(): Promise<void> {
+    await page.waitUntilElementIsClickableByLocator(nextBtn)
 }
