16189b1b399a8f2a68a64c04deafff2ec659581b
Eglė Venskūnaitė <egle.venskunaite@telesoftas.com>
2022-01-26 13:53:19 +0200

    add parameter types

--- web/pageObjects/resetPass.page.ts
+++ web/pageObjects/resetPass.page.ts
@@ -12,6 +12,6 @@ export async function getResetPassPageTitleText(): Promise<string> {
 
 //----------------WAIT----------------
 
-export async function waitForSendRecoveryCodeBtnInDOM(timeToWait?): Promise<void> {
+export async function waitForSendRecoveryCodeBtnInDOM(timeToWait?: number): Promise<void> {
     await page.waitUntilElementIsVisibleInDOMByLocator(sendRecoveryCodeBtn)
 }
