16e59b2346dccd221aec137a0a8d486d374b8a26
Eglė Venskūnaitė <egle.venskunaite@telesoftas.com>
2022-01-26 13:55:15 +0200

    add landing page title

--- web/utils/enums.ts
+++ web/utils/enums.ts
@@ -3,5 +3,6 @@ export enum HeaderText {
     signIn = 'Sign in',
     register = 'Register',
     discover = 'Discover',
-    resetPass = 'Reset your password'
+    resetPass = 'Reset your password',
+    landing = 'Play and create Quiz content'
 }
