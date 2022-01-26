5700322a4fcbcf89b47ed62d7417b09f33990b42
Eglė Venskūnaitė <egle.venskunaite@telesoftas.com>
2022-01-26 13:56:01 +0200

    add tests for sign in with facebook and change assertion for sign in with google case

--- web/tests/signInFacebook-e2e.ts
+++ web/tests/signInFacebook-e2e.ts
@@ -1,30 +1,31 @@
 import * as landingPage from '../pageObjects/landing.page'
 import { openLandingPage } from '../pageObjects/page'
-import * as signInGoogle from '../pageObjects/signInGoogle'
+import * as signInFacebook from '../pageObjects/signInFacebook'
 import { expect } from 'chai'
 import { waitForUserProfileImageInHeader } from '../pageObjects/header.element'
-import { userGoogle } from '../utils/userCredentials'
+import { userFacebook } from '../utils/userCredentials'
 import { getDiscoverPageTitleText } from '../pageObjects/discover.page'
 import { HeaderText } from '../utils/enums'
 
 
-describe('Continue with Google', () => {
+describe('Continue with Facebook', () => {
 
     beforeEach(async function (){
         await openLandingPage()        
     })
     
-    it('Initiate Continue with Google and return back to landing', async () => {
-        await landingPage.clickContinueWithGoogleBtn()
-        await signInGoogle.waitForNextBtnInGoogleIsClickable()
+    it('Initiate Continue with Facebook and return back to landing', async () => {
+        await landingPage.clickContinueWithFacebookBtn()
+        await signInFacebook.waitForConsentsPopupIsVisible(6000)
         await browser.back()
         await landingPage.waitForTermsAndConditionsLinkInViewport(8000)
         expect (await landingPage.getLandingPageTitleText()).equals(HeaderText.landing)
     })
 
-    it('Success Continue with Google login', async () => {
-        await landingPage.clickContinueWithGoogleBtn()
-        await signInGoogle.continueWithGoogle(userGoogle.email, userGoogle.pass)
+    it('Success Continue with Facebook login', async () => {
+        await landingPage.clickContinueWithFacebookBtn()
+        await signInFacebook.waitForConsentsPopupIsVisible(6000)
+        await signInFacebook.loginOnFacebook(userFacebook.email, userFacebook.pass)
         await landingPage.waitForTermsAndConditionsLinkInViewport(8000)
         await waitForUserProfileImageInHeader(8000)
         expect (await getDiscoverPageTitleText()).equals(HeaderText.discover)
