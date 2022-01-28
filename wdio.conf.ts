8cd03f543a20bbd937317fc327e0f0a36390271f
Eglė Venskūnaitė <egle.venskunaite@telesoftas.com>
2022-01-28 18:39:17 +0200

    chromeOptions introduce

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -57,6 +57,13 @@ export const config: WebdriverIO.Config = {
         maxInstances: 1,
         //
         browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: [
+                '--window-size=1920,1440',
+               // '--headless',
+               // '--remote-debugging-port=9222',
+            ],
+        },
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
