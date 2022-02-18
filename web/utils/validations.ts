33847190986d0fa1329521c0ef8ce166fc1c12a5
Eglė Venskūnaitė <egle.venskunaite@telesoftas.com>
2022-02-18 15:29:41 +0200

    add password reset validation message

--- web/utils/validations.ts
+++ web/utils/validations.ts
@@ -5,7 +5,9 @@ export enum Form {
     PassTooShort = 'Password must be at least of 6 characters in length',
     PassTooLong = 'Password must be at most of 100 characters in length',
     AlreadyRegisteredEmail = 'User with this email already exists.',
-    NoUserWithThisEmailErrorText = 'There is no user with this email address',
-    RecoveryCodeTooShortValidationErrorText = 'Recovery code must be at least 10 characters',
-    RecoveryCodeTooLongValidationErrorText = 'Recovery code must be at most 20 characters',
+    NoUserWithThisEmail = 'There is no user with this email address',
+    RecoveryCodeTooShort = 'Recovery code must be at least 10 characters',
+    RecoveryCodeTooLong = 'Recovery code must be at most 20 characters',
+    PasswordsMustMatch = 'Passwords must match',
+    IncorrectRecoveryCode = 'The recovery code is incorrect',
 }
