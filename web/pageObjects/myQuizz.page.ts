25b7fad28a6cd7daea225ea193b6a3e689ba615a
Eglė Venskūnaitė <egle.venskunaite@telesoftas.com>
2022-04-04 23:10:01 +0300

    add my quiz page

---
+++ web/pageObjects/myQuizz.page.ts
@@ -0,0 +1,44 @@
+import * as page from './page'
+import * as enums from '../utils/enums'
+
+
+const latestQuiz = '(//div[./div[@data-cy="quizCover"]]) [1]'
+
+const latestQuizPrice = `${latestQuiz} //div[@data-cy="desktopQuizPrice"] //div`
+const latestQuizTitle = `${latestQuiz} //h2[@data-cy="quizTitle"]`
+const latestQuizDesc = `${latestQuiz} //div[@data-cy="quizDescription"]`
+const latestQuizQuestionCount = `${latestQuiz} //div[@data-cy="quizQuestionCount"]`
+const latestQuizImage = `${latestQuiz} //div[@data-cy="quizCover"]`
+
+const moreOptBtn = '//button[@data-cy="moreOptionsButton"]'
+
+
+//----------------ACTION----------------
+
+export async function openMyCreatedQuizzes() {
+    await page.openMyCreatedQuizPage()
+    await page.waitUntilElementIsPresentByLocator(latestQuiz)
+}
+
+//----------------GET----------------
+
+export async function isQuizImageUploaded(): Promise<boolean> {
+    const attr = await (await page.getElementByLocator(latestQuizImage)).getAttribute('src') 
+    return attr.length > 0 ? true : false
+}
+
+export async function getQuizPrice(): Promise<string> {
+    return await page.getElementTextByLocator(latestQuizPrice)
+}
+
+export async function getQuizTitle(): Promise<string> {
+    return await page.getElementTextByLocator(latestQuizTitle)
+}
+
+export async function getQuizDescription(): Promise<string> {
+    return await page.getElementTextByLocator(latestQuizDesc)
+}
+
+export async function getQuizQuestionCount(): Promise<string> {
+    return await page.getElementTextByLocator(latestQuizQuestionCount)
+}
