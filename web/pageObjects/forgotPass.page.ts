b0832fbb9335f32c4d0fc0db8fdf74f8b0851eca
Eglė Venskūnaitė <egle.venskunaite@telesoftas.com>
2022-05-02 23:04:55 +0300

    reset pass refactor

--- web/pageObjects/forgotPass.page.ts
+++ web/pageObjects/forgotPass.page.ts
@@ -2,6 +2,7 @@ import * as page from './page'
 import * as enums from '../utils/enums'
 import * as userCredentials from '../utils/userCredentials'
 import * as mailApp from '../utils/mailApp'
+import * as defaultCredentials from '../../userCredentials.json'
 
 const backendFormValidationError = '//main //form/h3'
 
@@ -15,8 +16,8 @@ export async function getBackendValidationErrorTextAfterBtnIsClickable(btn: stri
 
 //----------------ACTION----------------
 
-export async function receiveRecoveryCodeWithEmail(email: string): Promise<void> {
-    await page.fillFormInputWithValue(enums.Input.Email, email)
+export async function receiveRecoveryCodeWithEmail(email?: string): Promise<void> {
+    await page.fillFormInputWithValue(enums.Input.Email, email || defaultCredentials.email)
     await page.clickOnFormButton(enums.Button.SendRecoveryCode)
 }
 
@@ -27,22 +28,22 @@ export async function fillRecoveryCode(recoveryCode: string): Promise<void> {
 }
 
 export async function registeredEmailWithInvalidRecoveryCode(): Promise<void> {
-    await receiveRecoveryCodeWithEmail(userCredentials.userForgotPass.email)
+    await receiveRecoveryCodeWithEmail(defaultCredentials.resetPasswordEmail)
     await page.waitUntilFormButtonByTextIsClickable(enums.Button.NextWithRecoveryCode, 4000)
     await fillRecoveryCode('1111111111')
-    await fillPassInputsAndSubmit(userCredentials.userForgotPass.newPass, userCredentials.userForgotPass.newPass)
+    await fillPassInputsAndSubmit()
 }
 
-export async function fillPassInputsAndSubmit(pass: string, repeatPass: string): Promise<void> {
-    await page.fillFormInputWithValue(enums.Input.Password, pass)
-    await page.fillFormInputWithValue(enums.Input.RepeatPass, repeatPass)
+export async function fillPassInputsAndSubmit(pass?: string, repeatPass?: string): Promise<void> {
+    await page.fillFormInputWithValue(enums.Input.Password, pass || defaultCredentials.resetPasswordPass)
+    await page.fillFormInputWithValue(enums.Input.RepeatPass, repeatPass || defaultCredentials.resetPasswordPass)
     await page.clickOnFormButton(enums.Button.DonePasswordReset)
 }
 
 export async function registeredEmailWithSuccessRecoveryCode(): Promise<void> {
-    await receiveRecoveryCodeWithEmail(userCredentials.userForgotPass.email)
+    await receiveRecoveryCodeWithEmail(defaultCredentials.resetPasswordEmail)
     const verificationCode = await mailApp.getTempPasswordFromEmail()
     await fillRecoveryCode(verificationCode)
     await page.waitUntilFormButtonByTextIsClickable(enums.Button.DonePasswordReset)
-    await fillPassInputsAndSubmit(userCredentials.userForgotPass.newPass, userCredentials.userForgotPass.newPass)
+    await fillPassInputsAndSubmit(defaultCredentials.resetPasswordPass, defaultCredentials.resetPasswordPass)
 }
